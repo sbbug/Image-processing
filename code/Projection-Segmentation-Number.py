b3def3bdfd74d0fff04dbcf9c8419526eb942260
hongyi <1103009779@qq.com>
2019-03-26 10:28:55 +0800

    添加数字分割

--- code/Projection-Segmentation-Number.py
+++ code/Projection-Segmentation-Number.py
@@ -5,6 +5,10 @@ import matplotlib.pyplot as plt
 
 #获取图像的二值图
 def getBinaryImg(file_path):
+    '''
+    :param file_path: 图像路径
+    :return: 返回处理后二值图
+    '''
     raw_img = cv.imread(file_path)
     gray_img = cv.cvtColor(raw_img, cv.COLOR_BGR2GRAY)
     ret, thresh = cv.threshold(gray_img, 100, 255, cv.THRESH_BINARY)
@@ -13,10 +17,15 @@ def getBinaryImg(file_path):
     return res
 
 #将图像进行投影压缩
-def getSplitLocation(img_bin):
-
+def getSplitLocation(img_bin,thre):
+    '''
+    :param img_bin: 图像二值图
+    :param thre: 阈值，大于该阈值则为数字区域
+    :return:
+    '''
     img_h,img_w = img_bin.shape
     img_vec = np.zeros(img_w,np.int32)
+    split_loc = []
 
     for i in range(img_w):
         col = img_bin[:,i]
@@ -24,18 +33,51 @@ def getSplitLocation(img_bin):
             if col[j]==255:
                 img_vec[i]=img_vec[i]+1
 
-    print(img_vec)
+    #寻找分割点
+    flag = True
+    start = -1
+    end = -1
+    for i in range(len(img_vec)):
+        if img_vec[i]>=thre and start==-1:
+            start=i
+            flag = False
+        elif img_vec[i]<thre and flag==False:
+            end = i
+            split_loc.append([start,end])
+            flag = True
+            start = -1
+            end = -1
+        else:
+            continue
+
+
     x = range(0,img_w)
     #绘制每一列的白色像素统计值
     plt.plot(x, img_vec, linewidth=2)
     plt.show()
 
+    return split_loc
+
 
 if __name__ =="__main__":
 
+    raw = cv.imread("../images/numbers.png")
     img_bin = getBinaryImg("../images/numbers.png")
-    getSplitLocation(img_bin)
+    img_h,img_w = img_bin.shape
+    split_locations = getSplitLocation(img_bin,20)
+    print(split_locations)
+    for i in range(len(split_locations)):
+        point = split_locations[i]
+        print(point)
+        # cv.circle(raw,(point[0],0),3,(0,0,255),0)
+        # cv.circle(raw,(point[1],0), 3, (0, 0, 255),0)
+        # #img_bin[0:point[1]-point[0],point[0]:img_h]
+        data = img_bin[0:img_h,point[0]:point[1]]
+        # print(data.shape)
+        data = cv.resize(data,(40,40))
+        cv.imwrite(str(i)+".jpg",data)
     cv.imshow("bin",img_bin)
+    cv.imshow("res", raw)
     # for i in range(len(img_bin)):
     #     for j in range(len(img_bin[i])):
     #         print(img_bin[i][j],end=" ")
